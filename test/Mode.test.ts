c2e577855d09ce2b5f4cf84459dd21975c42d640
jansel369 <dianzel555@gmail.com>
2017-11-15 08:34:33 +0800

    Model test

--- test/Mode.test.ts
+++ test/Mode.test.ts
@@ -2,6 +2,12 @@ import Model from '../lib/Model';
 
 describe('Model', () => {
     describe('constructor', () => {
-        it('accepts object as parameter and set as properties')
+        it('accepts object as parameter and set as properties', () => {
+            const model = new Model({ foo: 'foo', bar: 'bar' });
+
+            expect(model).toBeInstanceOf(Model);
+            expect((model as any).foo).toBe('foo');
+            expect((model as any).bar).toBe('bar');
+        });
     })
 });
